82c2517ef612456663453cb89a7d48be0a7b945d
Steven Deblois <sdeblois@itesmedia.tv>
2018-06-17 21:03:41 -0400

    Fix Elin forest NPCs throwing errors

--- scripts/npc/2131001.js
+++ scripts/npc/2131001.js
@@ -19,10 +19,10 @@ function action(mode, type, selection) {
 	    cm.sendNext("You don't have enough... I need at least 100.");
 	    cm.dispose();
 	} else {
-	    cm.sendGetNumber("Hey, that's a good idea! I can give you #i4310000#Perfect Pitch for each 100 #i" + exchangeItem + "##t" + exchangeItem + "# you give me. How many do you want? (Current Items: " + cm.getPlayer().itemQuantity(exchangeItem) + ")", java.lang.Math.min(300, cm.getPlayer().itemQuantity(exchangeItem) / 100), 1, java.lang.Math.min(300, cm.getPlayer().itemQuantity(exchangeItem) / 100));
+	    cm.sendGetNumber("Hey, that's a good idea! I can give you #i4310000#Perfect Pitch for each 100 #i" + exchangeItem + "##t" + exchangeItem + "# you give me. How many do you want? (Current Items: " + cm.itemQuantity(exchangeItem) + ")", java.lang.Math.min(300, cm.itemQuantity(exchangeItem) / 100), 1, java.lang.Math.min(300, cm.itemQuantity(exchangeItem) / 100));
 	}
     } else if (status == 2) { 
-	if (selection >= 1 && selection <= cm.getPlayer().itemQuantity(exchangeItem) / 100) {
+	if (selection >= 1 && selection <= cm.itemQuantity(exchangeItem) / 100) {
 	    if (!cm.canHold(4310000, selection)) {
 		cm.sendOk("Please make some space in ETC tab.");
 	    } else {
