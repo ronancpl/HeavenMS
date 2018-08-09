fc8eadec3012adb5f16f2d7f69ba8e1707d514bd
vince538 <39571654+vince538@users.noreply.github.com>
2018-08-09 03:31:34 +0200

    Update 1012119.js (#238)
    This should fix the Dc.

--- scripts/npc/1012119.js
+++ scripts/npc/1012119.js
@@ -24,16 +24,20 @@ function action(mode, type, selection) {
                 return;
         }
         
-	if (cm.isQuestActive(22515) || cm.isQuestActive(22516) || cm.isQuestActive(22517) || cm.isQuestActive(22518)) {
-		cm.sendYesNo("Would you like to go in the special Spore Training Center?");
+	else if (cm.isQuestActive(22515) || cm.isQuestActive(22516) || cm.isQuestActive(22517) || cm.isQuestActive(22518)) {
+		cm.sendYesNo("Would you like to enter the special Training Center?");
 		status = 1;
 	}
+
+else {
         
 	var selStr = "Would you like to go into the Training Center?";
 	for (var i = 0; i < num; i++) {
 		selStr += "\r\n#b#L" + i + "#Training Center " + i + " (" + cm.getPlayerCount(map + i) + "/" + maxp + ")#l#k";
 	}
 	cm.sendSimple(selStr);
+}
+
     } else if (status == 1) {
 	if (selection < 0 || selection >= num) {
 		cm.dispose();
