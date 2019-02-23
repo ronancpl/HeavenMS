c409d5511b56731a2b40f31893c46c12b018f5a7
Pandabyte <jerryw4113@gmail.com>
2019-02-23 14:37:20 +1000

    Added missing votepoints in sql and tweaked MapleClient

--- sql/db_database.sql
+++ sql/db_database.sql
@@ -40,6 +40,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `email` varchar(45) DEFAULT NULL,
   `ip` text,
   `rewardpoints` int(11) NOT NULL DEFAULT '0',
+  "votepoints" int(11) NOT NULL DEFAULT '0',
   `hwid` varchar(12) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
