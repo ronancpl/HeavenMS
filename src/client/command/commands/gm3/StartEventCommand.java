0dfd57c4ee4e479951cd19f3d049d0f2199dfd6c
Nulliphite <47942799+Nulliphite@users.noreply.github.com>
2019-02-28 03:04:27 +1000

    [QoL] Added broadcast message to !startevent command to increase expo… (#414)
    * [QoL] Added broadcast message to !startevent command to increase exposure
    
    * Change broadcastmessage to not just GM

--- src/client/command/commands/gm3/StartEventCommand.java
+++ src/client/command/commands/gm3/StartEventCommand.java
@@ -26,7 +26,9 @@ package client.command.commands.gm3;
 import client.command.Command;
 import client.MapleClient;
 import client.MapleCharacter;
+import net.server.Server;
 import server.events.gm.MapleEvent;
+import tools.MaplePacketCreator;
 
 public class StartEventCommand extends Command {
     {
@@ -40,6 +42,16 @@ public class StartEventCommand extends Command {
         if (params.length > 1)
             players = Integer.parseInt(params[0]);
         c.getChannelServer().setEvent(new MapleEvent(player.getMapId(), players));
-        player.dropMessage(5, "The event has been set on " + player.getMap().getMapName() + " and will allow " + players + " players to join.");
+        Server.getInstance().broadcastMessage(c.getWorld(), MaplePacketCreator.earnTitleMessage(
+                "[Event] An event has started on "
+                        + player.getMap().getMapName()
+                        + " and will allow "
+                        + players
+                        + " players to join. Type @joinevent to participate."));
+        player.dropMessage(5, "[Event] An event has started on "
+                + player.getMap().getMapName()
+                + " and will allow "
+                + players
+                + " players to join.");
     }
 }
