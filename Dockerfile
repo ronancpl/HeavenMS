63e222d13de34af8702d9405c2a2f798740eb1e7
kolakcc <kolakcc@gmail.com>
2019-07-15 15:17:49 +0200

    Remove NetBeans project files in favor of gradle files + updated docker support

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 # Docker support, thanks to xinyifly
 
-FROM openjdk:7-alpine
+FROM openjdk:8-alpine
 RUN apk -U add tini
 WORKDIR /mnt
-COPY ./ ./
-RUN sh ./posix-compile.sh
+ADD wz ./wz
+ADD docker-launch.sh configuration.ini world.ini ./
+ADD build/libs/HeavenMS.jar HeavenMS.jar
 EXPOSE 8484 7575 7576 7577
 CMD exec tini -- sh ./docker-launch.sh
--- docker-launch.sh
+++ docker-launch.sh
@@ -4,5 +4,6 @@ sed -i "s/HOST=.*/HOST=${HOST}/" configuration.ini
 sed -i "s|URL=.*|URL=${URL}|" configuration.ini
 sed -i "s/DB_USER=.*/DB_USER=${DB_USER}/" configuration.ini
 sed -i "s/DB_PASS=.*/DB_PASS=${DB_PASS}/" configuration.ini
+sed -i "s/JAVA8=.*/JAVA8=TRUE/" configuration.ini
 
-exec sh ./posix-launch.sh
+exec java -Xmx2048m -Dwzpath=wz -jar HeavenMS.jar
